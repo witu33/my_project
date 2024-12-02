7bc66b5c6474e7a62b8a973fd00353f07abb86b7
witu33 W <witbartlomiej1@gmail.com>
2024-12-02 17:51:39 +0000

    Created msg storage

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -1,4 +1,26 @@
-#[ic_cdk::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
+use std::cell::RefCell
+
+thread_local! {
+    static msg: RefCell<String> = RefCell::new(String::new());
 }
+fn get() {
+    let a =String::from("123");
+    let b =a.clone();
+    let c=a;
+    msg.with(|msg| (*msg.borrow()).clone())
+}
+
+#[ic_cdk::query]
+fn get_msg() -> String {
+    msg.with(|msg| (*msg.borrow()).clone())
+}
+
+#[ic_cdk::update]
+fn set_msg(new_msg: String) {
+    msg.with(|msg| (*msg.borrow_mut).clone())
+}
+fn set(n: Nat) {
+    // COUNTER.replace(n);  // requires #![feature(local_key_cell_methods)]
+    COUNTER.with(|count| *count.borrow_mut() = n);
+}
+
