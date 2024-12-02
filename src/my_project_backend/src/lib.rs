b1a668df32c53076ba5e69b61855023b31570a96
witu33 W <witbartlomiej1@gmail.com>
2024-12-02 18:09:36 +0000

    ap

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -1,26 +1,29 @@
-use std::cell::RefCell
+use std::cell::RefCell;
 
 thread_local! {
-    static msg: RefCell<String> = RefCell::new(String::new());
-}
-fn get() {
-    let a =String::from("123");
-    let b =a.clone();
-    let c=a;
-    msg.with(|msg| (*msg.borrow()).clone())
+    static MSG: RefCell<String> = RefCell::new(String::new());
 }
 
+
 #[ic_cdk::query]
-fn get_msg() -> String {
-    msg.with(|msg| (*msg.borrow()).clone())
+fn get_msg() -> String{
+    MSG.with(|msg| msg.borrow().clone())
 }
 
 #[ic_cdk::update]
 fn set_msg(new_msg: String) {
-    msg.with(|msg| (*msg.borrow_mut).clone())
-}
-fn set(n: Nat) {
-    // COUNTER.replace(n);  // requires #![feature(local_key_cell_methods)]
-    COUNTER.with(|count| *count.borrow_mut() = n);
+    MSG.with(|msg| *msg.borrow_mut() = new_msg)
 }
 
+// new_msg: String
+// msg.borrow_mut(): &mut String
+// &mut String != String ???
+// *(&mut String) == String
+
+
+
+//COUNTER.with(|count| *count.borrow_mut() = n);
+#[ic_cdk::query]
+fn greet(name: String) -> String {
+    format!("Hello, {}!", name)
+}
